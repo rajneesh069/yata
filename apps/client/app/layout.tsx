3138dd3b064d0d2c045ef7cde8b2be23fb006ea1
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-02-21 19:21:23 +0530

    add clerkjs for auth

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,30 +1,47 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/providers";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
-const fontSans = Geist({
+const inter = Inter({
   subsets: ["latin"],
-  variable: "--font-sans",
-})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
+  variable: "--font-inter",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
+      <body className={`${inter.variable} font-inter antialiased h-[100vh]`}>
+        <Providers>
+          <header className="flex justify-end items-center p-4 gap-4 h-[8vh]">
+            {/* Show the sign-in and sign-up buttons when the user is signed out */}
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            {/* Show the user button when the user is signed in */}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <div className="h-[92vh]">{children}</div>
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
