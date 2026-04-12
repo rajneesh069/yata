b0fc7a06cd4247cea8516e83b548a978af48f4b5
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-04-12 19:59:09 +0530

    Changes: 1. Modify create_organizations_table.up.sql file, c hange the org_name field to name and creator_id now  has inline FK instead of an explicit constraint.
    Additions:
    1. Create a users_organizations join table to accomodate the many to many relationships between the users and the organizations.
    2. Create the down migration file in order for it as well.

--- apps/server/migrations/000003_create_organizations_table.up.sql
+++ apps/server/migrations/000003_create_organizations_table.up.sql
@@ -1,18 +1,12 @@
 CREATE TABLE IF NOT EXISTS organizations(
     id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     clerk_org_id VARCHAR(255) UNIQUE NOT NULL,
-    org_name VARCHAR(255) NOT NULL CHECK(char_length(trim(org_name)) > 0),
+    name VARCHAR(255) NOT NULL CHECK(char_length(trim(name)) > 0),
     slug VARCHAR(255) UNIQUE NOT NULL CHECK(char_length(trim(slug)) > 0),
-    creator_id UUID NOT NULL,
+    creator_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     image_url VARCHAR(500) NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-
-    CONSTRAINT fk_creator_id
-    FOREIGN KEY (creator_id)
-    REFERENCES users(id)
-    ON DELETE CASCADE
-
 );
 
 CREATE TRIGGER updated_org_modtime
---
+++ apps/server/migrations/000004_create_user_organizations_join_table.down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER IF EXISTS updated_user_organizations ON user_organizations;
+
+DROP TABLE IF EXISTS user_organizations;
+
+DROP TYPE IF EXISTS role;
