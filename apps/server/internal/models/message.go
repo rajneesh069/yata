fce68f4818a8a53876c9fd4dace5de5b2f033065
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-04-19 16:01:30 +0530

    refactor(message): modify Message struct according to new DB schema
    1. Move from string to uuid.UUID for id fields.
    2. Remove SentByID, TicketID, ReceivedByID, Body, AttachmentID, EditedAt
    fields as they didn't make sense in accordance with the new schema.
    3. Add ConversationID, ParentMessageID for reply access, DeletedAt,
    SentBy.

--- apps/server/internal/models/message.go
+++ apps/server/internal/models/message.go
@@ -1,15 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Message struct {
-	ID           string    `json:"id" db:"id"`
-	SentByID     string    `json:"sentById" db:"sent_by_id"`
-	TicketID     *string   `json:"ticketId" db:"ticket_id"`
-	ReceivedByID string    `json:"receivedById" db:"received_by_id"` // could be a user id or a ticket id, depending on the context of where was it sent, in the ticket chat or private chat, if it was sent in the ticket chat ReceivedByID == TicketID, or else ReceivedByID == UserID
-	Body         *string   `json:"body" db:"body"`
-	AttachmentID *string   `json:"attachmentId" db:"attachment_id"`
-	IsEdited     bool      `json:"isEdited" db:"is_edited"`
-	SentAt       time.Time `json:"sentAt" db:"sent_at"`
-	EditedAt     time.Time `json:"editedAt" db:"edited_at"`
+	ID              uuid.UUID `json:"id" db:"id"`
+	Content         string    `json:"content" db:"content"`
+	SentBy          uuid.UUID `json:"sentBy" db:"sent_by"`
+	ConversationID  uuid.UUID `json:"conversationId" db:"conversation_id"`
+	ParentMessageID uuid.UUID `json:"parentMessageId" db:"parent_message_id"`
+	SentAt          time.Time `json:"sentAt" db:"sent_at"`
+	DeletedAt       time.Time `json:"deletedAt" db:"deleted_at"`
 }
