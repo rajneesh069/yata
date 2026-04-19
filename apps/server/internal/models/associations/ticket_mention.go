8fb0af205eefccc2e751b1f4d9339f1bcd2b343e
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-04-19 12:57:47 +0530

    move from string type to uuid for id fields using google's uuid package

--- apps/server/internal/models/associations/ticket_mention.go
+++ apps/server/internal/models/associations/ticket_mention.go
@@ -1,9 +1,13 @@
 package associations
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type TicketMention struct {
-	TicketID    string    `json:"ticketId" db:"ticket_id"` // FK to ticket.id
-	UserID      string    `json:"userId" db:"user_id"`     // FK to user.id
+	TicketID    uuid.UUID `json:"ticketId" db:"ticket_id"` // FK to ticket.id
+	UserID      uuid.UUID `json:"userId" db:"user_id"`     // FK to user.id
 	MentionedAt time.Time `json:"mentionedAt" db:"mentioned_at"`
 }
