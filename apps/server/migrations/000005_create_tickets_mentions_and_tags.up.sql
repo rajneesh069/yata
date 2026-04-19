fda1d19cb985c3baea9408a637290e28b569e0b0
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-04-19 16:12:54 +0530

    refactor(migrations): modify db migrations in accordance with new schema

--- apps/server/migrations/000005_create_tickets_mentions_and_tags.up.sql
+++ apps/server/migrations/000005_create_tickets_mentions_and_tags.up.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS mentions(
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     ticket_id UUID NOT NULL REFERENCES tickets(id) ON DELETE CASCADE,
 
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    mentioned_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY(user_id, ticket_id)
 );
 
--- apps/server/migrations/000007_create_attachments.up.sql
+++ apps/server/migrations/000007_create_attachments.up.sql
@@ -2,14 +2,13 @@ CREATE TABLE IF NOT EXISTS attachments(
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     url TEXT NOT NULL,
     message_id UUID NOT NULL REFERENCES messages(id) ON DELETE CASCADE,
+    name VARCHAR(300) NULL,
     mime_type VARCHAR(20) NOT NULL,
     size_in_bytes NUMERIC NOT NULL,
     width INT DEFAULT NULL,
     height INT DEFAULT NULL,
-    length_minutes INT DEFAULT NULL,
     length_seconds INT DEFAULT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    uploaded_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 );
 
 CREATE INDEX idx_attachments_ticket_id ON attachments(message_id);
