8272db62a0347240f0b91f37f61e8e630f3d32c9
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2026-04-19 15:57:23 +0530

    refactor(attachments): modify fields according to new schema
    1. Move from string to uuid.UUID for the ID fields.
    2. Add a Name field for filename which could be null.
    3. Change the field name Size to SizeInBytes for explicit 'unit' hint.
    4. Remove the field name UploadedByID and replace it with MessageID as
    per the new schema.
    5. Change the DurationInSeconds Field to LengthSeconds field to store a
    video's duration.
    6. Remove ThumbnailURL for now.

--- apps/server/internal/models/attachment.go
+++ apps/server/internal/models/attachment.go
@@ -1,22 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
 
-// Message ID can never be nil except when the attachment is the part of a ticket.
+	"github.com/google/uuid"
+)
 
 type Attachment struct {
-	ID                  string    `json:"id" db:"id"`
-	FileName            string    `json:"fileName" db:"file_name"`
-	AttachmentURL       string    `json:"attachmentUrl" db:"attachment_url"`
-	Size                int64     `json:"size" db:"size"`
-	UploadedByID        string    `json:"uploadedById" db:"uploaded_by_id"` // FK to user.id
-	TicketID            *string   `json:"ticketID" db:"ticket_id"`          // FK to ticket.id
-	MessageID           *string   `json:"messageId" db:"message_id"`        // FK to message.id
-	MIMEType            string    `json:"mimeType" db:"mime_type"`
-	Width               *int      `json:"width" db:"width"`
-	Height              *int      `json:"height" db:"height"`
-	DurationInSeconds   *int      `json:"durationInSeconds" db:"duration_in_seconds"`
-	ThumbnailURL        *string   `json:"thumbnailUrl" db:"thumbnail_url"`
-	CodeSnippetLanguage *string   `json:"codeSnippetLanguage" db:"code_snippet_language"`
-	UploadedAt          time.Time `json:"uploadedAt" db:"uploaded_at"`
+	ID            uuid.UUID `json:"id" db:"id"`
+	URL           string    `json:"Url" db:"url"`
+	Name          *string   `json:"name" db:"name"`
+	SizeInBytes   int64     `json:"sizeInBytes" db:"size_in_bytes"`
+	MessageID     uuid.UUID `json:"messageId" db:"message_id"` // FK to message.id
+	MIMEType      string    `json:"mimeType" db:"mime_type"`
+	Width         *int      `json:"width" db:"width"`
+	Height        *int      `json:"height" db:"height"`
+	LengthSeconds *int      `json:"lengthSeconds" db:"length_seconds"`
+	UploadedAt    time.Time `json:"uploadedAt" db:"uploaded_at"`
 }
